572834b19746ccd0e527a2ac681585ab59fa3f56
LaZy1220 <4elove4iska@gmail.com>
2023-08-11 19:34:22 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const numberString = n.toString();
+    let reversedString = "";
+
+    for (let i = numberString.length - 1; i >= 0; i--) {
+        reversedString += numberString[i];
+    }
+
+    return parseInt(reversedString);
+};
